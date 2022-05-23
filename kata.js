dddfe258b755e66f3870dddebfe3d34dd6afc407
Maïté Armengaud <53378428+Ma-ho@users.noreply.github.com>
2022-05-23 13:31:38 +0200

    add conditions

--- kata.js
+++ kata.js
@@ -1,67 +1,87 @@
-// Trouver un nb compris entre 1 et 100
-
-// Fin si nb trouvé
-// Fin si essai supérieur ou égal à 10
-// Choisir un nb aléatoirement 
-// Demander au joueur de rentrer un nb
-// Si nb sup lui indiquer qu'il est trop grand
-//.. à décomposer plus
-// Si nb inf lui indiquer qu'il est trop bas
-//.. à décomposer plus
-// Conserver les propositions du joueur et lui retourner à chaque nouveau tour si il a faux
-// Permettre au joueur de recommencer
-
 let number;
+
+function StopTheGame() {
+    window.alert("La partie est finie");
+    //Proposer de recommencer ou non
+}
+
+ function PlayTheGame() {
+    number = window.prompt('Saisir un entier');
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 function chooseNumber() {
-    //numberRandom = getRandomInt(101);
-    numberRandom = 3;
+    numberRandom = getRandomInt(101);
+    //numberRandom = 3;
     console.log("numberRandom =", numberRandom);
     console.log("myNumber =", number);
 
-    nbTrial = 3; //à incrémenter à chaque essai
+    nbTrial = 10; //à "décrémenter" à chaque essai
     arrayNumber = [];
 
     while ( nbTrial > 0 ) {
         console.log("numberRandom =", numberRandom);
         console.log("myNumber =", number);
 
-        if ( number > numberRandom ) {
-            nbTrial == nbTrial --;
-            arrayNumber.push(number);
-            console.log("Votre nombre est trop grand - Vos numéros déjà utilisés", arrayNumber.join(' '), "- essais restants", nbTrial);
+        // Vérifier si j'ai un nb en entrée
+        if (!isNaN(number) && 0 < number && number < 101) {
 
-        } else if ( number < numberRandom ) {
-            nbTrial == nbTrial --;
-            arrayNumber.push(number);
-            console.log( "Votre nombre est trop petit - Vos numéros déjà utilisés", arrayNumber.join(' '), "- essais restants", nbTrial);
+            // si number trop grand
+            if ( number > numberRandom ) {
+                nbTrial == nbTrial --;
+                arrayNumber.push(number);
+                console.log("Votre nombre est trop grand - Vos numéros déjà utilisés", arrayNumber.join(' '), "- essais restants", nbTrial);
+                window.alert("Votre nombre est trop grand - Vos numéros déjà utilisés " + arrayNumber.join(' ') + " Nb d'essais restants " + nbTrial);
 
+            // si number trop petit
+            } else if ( number < numberRandom ) {
+                nbTrial == nbTrial --;
+                arrayNumber.push(number);
+                console.log("Votre nombre est trop petit - Vos numéros déjà utilisés", arrayNumber.join(' '), "- essais restants", nbTrial);
+                window.alert("Votre nombre est trop petit - Vos numéros déjà utilisés " + arrayNumber.join(' ') + '' +  " Nb d'essais restants " + nbTrial);
+
+            // si number trouvé
+            } else {
+                console.log("Super vous avez trouvé le bon numéro :", number);
+                window.alert("Super vous avez trouvé le bon numéro : " + number);
+                nbTrial == 3;
+                StopTheGame();
+                // Arrêter la partie et proposer de recommencer une partie ou non
+            }
+    
+            PlayTheGame()
+            console.log("arrayNumber", arrayNumber);
+
+        // Si number n'est pas un entier -> alerter, incrémenter le compteur d'essais et continuer la partie.
         } else {
-            console.log("Super vous avez trouvé le bon numéro :", number);
-            //proposer de recommencer une partie
-            nbTrial == 10;
-        }
+            nbTrial == nbTrial --;
+            window.alert("Veuillez entrer un entier compris entre 0 et 100");
 
-        number = window.prompt('Saisi un numéro');
-        console.log("arrayNumber", arrayNumber);
+            if (nbTrial > 0 ) {
+                PlayTheGame()
+            } else {
+                console.log("Looser, le nombre d'essai est expirééééééé");
+                 // Proposer de recommencer ou non
+            }
+        }
     }
     
     if ( nbTrial === 0 ) {
-        //window.alert("Looser, le nombre d'essai est expiré");
+        window.alert("Looser, le nombre d'essai est expiré");
         console.log("Looser, le nombre d'essai est expiré");
-        //proposer de recommencer une partie
+        // Proposer de recommencer ou non
     }   
 }
 
 
 //window.prompt('Saisi un numéro')
-number = window.prompt('Saisi un numéro');
+// function PlayTheGame() {
+//     number = window.prompt('Saisir un entier');
+// }
+number = window.prompt('Saisir un entier, vous avez trois essais');
 console.log(chooseNumber())
 
-//vérifier si c'est un numéro et non uns string
-//vérifier si number compris entre 1 et 100
 // proposer de rejouer
